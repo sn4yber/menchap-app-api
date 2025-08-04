e9e8ebe7a41cefa5b48e0e8bcadf377318716c22
sn4yber <snayber333@gmail.com>
2025-08-04 00:21:00 -0500

    fin

--- src/main/java/com/snayber/api_jdbc/ApiJdbcApplication.java
+++ src/main/java/com/snayber/api_jdbc/ApiJdbcApplication.java
@@ -2,12 +2,11 @@ package com.snayber.api_jdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ApiJdbcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiJdbcApplication.class, args);
 	}
-
 }
