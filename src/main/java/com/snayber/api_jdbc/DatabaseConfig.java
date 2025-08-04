76bead342d821973f31153964d17d992d42006a8
sn4yber <snayber333@gmail.com>
2025-08-03 23:18:55 -0500

    cambios

--- src/main/java/com/snayber/api_jdbc/DatabaseConfig.java
+++ src/main/java/com/snayber/api_jdbc/DatabaseConfig.java
@@ -1,19 +1,25 @@
 package com.snayber.api_jdbc;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @Configuration
 public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+    
+    @Value("${spring.datasource.username}")
+    private String user;
+    
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
     public Connection conexion() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "postgres";
-        String password = "456789";
         return DriverManager.getConnection(url, user, password);
     }
-
 }
--- src/main/java/com/snayber/api_jdbc/LoginService.java
+++ src/main/java/com/snayber/api_jdbc/LoginService.java
@@ -1,34 +1,27 @@
 package com.snayber.api_jdbc;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.snayber.api_jdbc.UsuarioRepository;
-import com.snayber.api_jdbc.Usuario;
-
 import java.sql.*;
 
 @Service
 public class LoginService {
-    private final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private final String USER = "postgres";
-    private final String PASSWORD = "456789";
+    @Autowired
+    private Connection conexion;
 
     public boolean validarCredenciales(String usuario, String contrasena) {
         String sql = "SELECT * FROM usuarios WHERE username = ? AND password = ?";
 
-        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-
+        try (PreparedStatement stmt = conexion.prepareStatement(sql)) {
             stmt.setString(1, usuario);
             stmt.setString(2, contrasena);
 
             try (ResultSet rs = stmt.executeQuery()) {
-                return rs.next(); // Si hay un resultado, las credenciales son correctas
+                return rs.next();
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
-
 }
